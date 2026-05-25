45aadcd1cc989d3739cb1a3ba6252cbd43783ca9
Vlopes7 <viniciuslopes242005@gmail.com>
2026-05-25 15:46:24 -0300

    Inserção de POST

--- CRUD mundo/backend/src/server.ts
+++ CRUD mundo/backend/src/server.ts
@@ -1,9 +1,16 @@
 import express from "express";
 import cors from "cors";
-import { PrismaClient } from '@prisma/client';
+import "dotenv/config";
+
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
 const app = express();
-const prisma = new PrismaClient();
 
 app.use(cors());
 app.use(express.json());
@@ -12,6 +19,31 @@ app.get("/", (req, res) => {
   res.json({
     message: "servidor rodando"
   });
+});
+
+app.post("/continentes", async (req, res) => {
+  try {
+    const { nome } = req.body;
+
+    const continente = await prisma.continente.create({
+      data: {
+        nome
+      }
+    });
+
+    res.status(201).json(continente);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao criar continente" });
+  }
+});
+
+app.get("/continentes", async (req, res) => {
+  try {
+    const continentes = await prisma.continente.findMany();
+    res.json(continentes);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao buscar continentes" });
+  }
 });
 
 const PORT = 3000;
